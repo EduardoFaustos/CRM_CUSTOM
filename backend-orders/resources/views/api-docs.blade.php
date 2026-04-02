6de61d8c49c1340f86e1dba07bf1513bd33406cd
Eduardo Faustos <edyfan@hotmail.com>
2026-04-01 22:02:12 -0500

    validacion de cambio de swagger a scalar en laravel

--- backend-orders/resources/views/api-docs.blade.php
+++ backend-orders/resources/views/api-docs.blade.php
@@ -3,46 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRM Orders API - Swagger UI</title>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css">
+    <title>CRM Orders API - Scalar</title>
     <style>
-        html {
-            box-sizing: border-box;
-            overflow: -moz-scrollbars-vertical;
-            overflow-y: scroll;
-        }
-
-        *, *:before, *:after {
-            box-sizing: inherit;
-        }
-
         body {
             margin: 0;
-            background: #fafafa;
+            padding: 0;
         }
     </style>
 </head>
-
 <body>
-<div id="swagger-ui"></div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.js"></script>
-<script>
-    window.onload = function () {
-        const ui = SwaggerUIBundle({
-            url: "/api/swagger-docs.json",
-            dom_id: '#swagger-ui',
-            deepLinking: true,
-            presets: [
-                SwaggerUIBundle.presets.apis,
-                SwaggerUIBundle.SwaggerUIStandalonePreset
-            ],
-            plugins: [
-                SwaggerUIBundle.plugins.DownloadUrl
-            ],
-            layout: "StandaloneLayout"
-        })
-        window.ui = ui
-    }
-</script>
+    <script
+        id="api-reference"
+        data-url="/api/swagger-docs.json">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
 </body>
 </html>
